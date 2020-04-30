f29772d19f89a8d80246e811b9e651198168b8c5
neelam <63499417+neelammehta-2020@users.noreply.github.com>
2020-04-30 17:23:32 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-    platform.display();
+   
 
 }
 
@@ -61,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
